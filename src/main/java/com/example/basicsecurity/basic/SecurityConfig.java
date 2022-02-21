a82c7392bcdc4669f4b8b49406b947e17619e4fd
injinjeong <snowwwwww@naver.com>
2022-02-21 21:27:22 +0900

    csrf

--- src/main/java/com/example/basicsecurity/basic/SecurityConfig.java
+++ src/main/java/com/example/basicsecurity/basic/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .formLogin();
 
+        //로그아웃 설정
         http
                 .logout()
                 .logoutUrl("/logout")
@@ -50,11 +51,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 })
                 .deleteCookies("remember-me")
                 ;
+        //remember-me 설정
         http
                 .rememberMe()
                 .rememberMeParameter("remember")
                 .tokenValiditySeconds(3600)
                 .userDetailsService(userDetailsService)
                 ;
+        //동시 세션 제어
+        http
+                .sessionManagement()
+                .maximumSessions(1) //최대 허용 가능 세션 수, -1: 무제한 로그인 세션 허용
+                .maxSessionsPreventsLogin(false) //동시 로그인 차단함,true:신규 로그인 -> 실패함,  false: 기존 세션 만료(default)
+                .expiredUrl("/expired") //세션이 만료된 경우 이동 할 페이지
+                ;
+        /**
+         * CSRF (사이트 간 요청 위조)
+         * 모든 요청에 랜덤하게 생성된 토큰을 HTTP 파라미터로 요구.
+         * 요청 시 전달되는 토큰 값과 서버에 저장된 실제 값과 비교한 후 만약 일치하지 않으면 요청은 실패한다.
+         */
+
+        http.csrf(); //기본 활성화 되어 있음
+//        http.csrf().disable(); //비 활성화
+
+
     }
 }
